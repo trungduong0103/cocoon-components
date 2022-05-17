b15c52ba9c48821aad8d2379181cc16f7c7ea12b
trung.duong <trung.duong@parcelperform.com>
2022-05-17 21:18:34 +0700

    Try not to use module css

--- dist/button.js
+++ dist/button.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import cn from 'classnames';
 
 var classes = ".button {\n  background-color: teal;\n  color: white;\n  font-weight: 700;\n  padding: 10px 15px;\n}";
 
 function Button({ onClick , children , label  }) {
     return /*#__PURE__*/ React.createElement("button", {
-        className: cn(classes.button),
+        className: classes.button,
         onClick: onClick
     }, children && children, label);
 }
--- src/button/Button.jsx
+++ src/button/Button.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import classes from "./Button.scss";
-import cn from "classnames";
 
 function Button({ onClick, children, label }) {
   return (
-    <button className={cn(classes.button)} onClick={onClick}>
+    <button className={classes.button} onClick={onClick}>
       {children && children}
       {label}
     </button>
